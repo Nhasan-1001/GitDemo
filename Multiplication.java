17d8a5482af039d9cb1aeae75ceef5cf6d4f5513
Nur <stknurhasan@gmail.com>
2022-07-12 21:32:18 +0530

    Initial Commit

---
+++ Multiplication.java
@@ -0,0 +1,8 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        int mul = a * b;
+        System.out.println("Multiplication of the two given number is: " +mul);
+    }
+}
