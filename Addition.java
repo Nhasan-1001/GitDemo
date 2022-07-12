82df22154646106015bafa77ba742c20871a650a
Nur <stknurhasan@gmail.com>
2022-07-12 21:34:58 +0530

    Second Commit

--- Addition.java
+++ Addition.java
@@ -1,7 +1,7 @@
 public class Addition {
     public static void main(String[] args) {
         int a = 10;
-        int b = 20;
+        int b = 24;
         int sum = a + b;
         System.out.println("Sum of the two given number is: " +sum);
     }
